d4d5403a59f690c2f60a76a5c594673c30be2b8a
sedrik test <sedrk2401@gmal.com>
2023-02-15 20:35:02 +0300

    add commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Неттология!!!");
+        System.out.println(5+5);
+
+        int speed;
+    }
+}
